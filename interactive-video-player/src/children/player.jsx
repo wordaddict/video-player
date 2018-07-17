0cb4ecf2fe9b85f75117fa627090e2db13915dcb
wordaddict <madeyinka6@gmail.com>
2018-07-17 16:55:55 +0100

    added video component

--- interactive-video-player/src/children/player.jsx
+++ interactive-video-player/src/children/player.jsx
@@ -3,27 +3,35 @@ import '../App.css';
 
 class Players extends Component {
     constructor(){
-
+        super(this.props)
+        this.state = event
     }
+  getElements() {
+    this.togglePlay();
+    const player = document.querySelector('.player');
+    const video = player.querySelector('.viewer');
+    const progress = player.querySelector('.progress');
+    const progressBar = player.querySelector('.progress__filled');
+    const toggle = player.querySelector('.toggle');
+    const skipButtons = player.querySelectorAll('[data-skip]');
+    const ranges = player.querySelectorAll('.player__slider');
+  }
 
-    getElements() {
-        const player = document.querySelector('.player');
-        const video = player.querySelector('.viewer');
-        const progress = player.querySelector('.progress');
-        const progressBar = player.querySelector('.progress__filled');
-        const toggle = player.querySelector('.toggle');
-        const skipButtons = player.querySelectorAll('[data-skip]');
-        const ranges = player.querySelectorAll('.player__slider');
-    }
+  handleClick = (event) => {
+      this.props.children()
+  }
 
-    togglePlay() {
-        const method = video.paused ? 'play' : 'pause';
-        video[method]();
-      }
+  togglePlay() {
+    this.getElements();
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+  }
 
-    render() {
-        return (
-            this.togglePlay()
-        );
-    }
+  render() {
+    return (
+      this.togglePlay()
+    );
+  }
 }
+
+module.export = Players;
